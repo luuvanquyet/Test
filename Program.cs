7b34a34e4c0c9548e64c4541e5d92a169a5dfec1
luuvanquyet <luuvanquyet1998@gmail.com>
2021-08-22 23:41:30 +0700

    Init Project

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AutoCopy
+{
+    static class Program
+    {
+
+        [STAThread]
+
+        static void Main()
+        {
+            string CopyDate;
+            string SrcPath,DesPath,lastPath ;
+            int Count = 0;
+            CopyDate = DateTime.Now.Date.AddDays(-1).ToString("yyyyMMdd");
+            DesPath = @"D:\Test\Desc";
+            SrcPath = @"D:\Test\Src";
+            try
+            {
+                //Check Source Folder exists or not
+                //If exists
+                if (Directory.Exists(SrcPath))
+                {
+                    string[] folderPaths = Directory.GetDirectories(SrcPath);
+                    foreach(var item in folderPaths)
+                    {
+                        int index = item.LastIndexOf("\\");
+                        lastPath = item.Substring(index);
+                        //Kiem tra thu muc dich da co hay chua
+                        if (!Directory.Exists(DesPath+lastPath))
+                        {
+                            Directory.CreateDirectory(DesPath + lastPath);
+                        }
+                        //Tien hanh coppy du lieu
+                        string[] filePaths = Directory.GetFiles(item);
+                        foreach (var filename in filePaths)
+                        {
+                            File.Copy(filename, DesPath+ lastPath+ "\\" + Path.GetFileName(filename), true);
+                            Count++;
+                        }
+                        WriteLog.Write("Copy data OK!!" + Environment.NewLine + "Source path: " + item);
+                    }
+            
+                    //Check if copy file success or not
+                    if (Count > 0)
+                    {
+                        WriteLog.Write("Copy data completed successfully!!" + Environment.NewLine + "File copied: " + Count);
+                        Application.Exit();
+                        
+                    }
+                    else
+                    {
+                        WriteLog.Write("Copy data is not exist!!");
+                        Application.Exit();                        
+                    }
+                }
+                else
+                {
+                    WriteLog.Write("Folder is not exists!!");
+                    Application.Exit();
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLog.Write(ex.ToString());
+                Application.Exit();               
+            }
+        }      
+        
+    }
+}
---
+++ WriteLog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCopy
+{
+    public class WriteLog
+    {
+        public static string _Path = string.Empty;
+        private static bool DEBUG = true;
+
+        public static void Write(string msg)
+        {
+            _Path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            //_Path = @"\\fstvn01\Data\00_KDTVN Common(KDTVN共通)\⑥Parts Quality Control(部品ＱＣ)\Supplier  Data\log\";
+            try
+            {
+                using (StreamWriter w = File.AppendText(Path.Combine(_Path, "LOG.txt")))
+                {
+                    Log(msg, w);
+                }
+                if (DEBUG)
+                    Console.WriteLine(msg);
+            }
+            //catch (Exception e)
+            catch
+            {
+                //Handle
+            }
+        }
+
+        static private void Log(string msg, TextWriter w)
+        {
+            try
+            {
+                w.Write(Environment.NewLine);
+                w.Write("[{0} {1}]", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+                w.Write("\t");
+                w.WriteLine(" {0}", msg);
+                w.WriteLine("-----------------------");
+            }
+            //catch (Exception e)
+            catch
+            {
+                //Handle
+            }
+        }
+    }
+}
